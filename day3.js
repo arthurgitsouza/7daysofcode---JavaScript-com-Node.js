7a99382c7108b590d64c1ba1899122e84a660394
Joao Arthur Pereira de Souza <JT0075306@MATEUS.DC>
2025-05-20 13:16:15 -0300

    up 3day

--- day3.js
+++ day3.js
@@ -11,16 +11,31 @@ const front = 'Front-End'
 rl.question('Qual o seu nome? ', (nome) =>{
     rl.question(`${nome}, qual área você quer seguir? (${back}|${front})? `, (op1) => {
         if (op1 == front){
-            rl.question('Você quer aprender REACT ou VUE?', (op2) =>{
+            rl.question('Você quer aprender REACT ou VUE ?', (op2) =>{
                 rl.question(`Você quer seguir se especializando na área de ${front} ou seguir se desenvolvendo para se tornar Fullstack? `, (op3) => {
+                    perguntaTecnologias()
                 })
-                })
+            })
             } else if (op1 == back){
                 rl.question('Você quer aprender C# ou JAVA? ', (op2) => {
-                    rl.question(`Você quer seguir se especializando na área de ${back} ou seguir se desenvolvendo para se tornar Fullstack`, (op3) =>{
+                    rl.question(`Você quer seguir se especializando na área de ${back} ou seguir se desenvolvendo para se tornar Fullstack? `, (op3) =>{
+                        perguntarTecnologias()
                     })
                 })
         }
-        }
     })
 })
+
+function perguntarTecnologias(){
+    rl.question('Tem mais alguma tecnologia que você gostaria de aprender? (Digite "ok" para sim ou "não" para encerrar): ', (resposta) => {
+        if (resposta.toLowerCase() == 'ok'){
+            rl.question('Qual tecnologia você gostaria de aprender? ', (tecnologia) => {
+                console.log(`Legal! ${tecnologia} parece ser uma ótima tecnologia para aprender!`);
+                perguntarTecnologias()
+            });
+        } else {
+            console.log('Obrigado por compartilhar suas preferências! Boa jornada nos estudos!');
+            rl.close()
+        }
+    })
+}
