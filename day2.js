93eb4c2a07b807764e6f5edd4f9924fee87d4992
Joao Arthur Pereira de Souza <JT0075306@MATEUS.DC>
2025-05-20 11:48:42 -0300

    first commit

---
+++ day2.js
@@ -0,0 +1,21 @@
+const readline = require('readline') // Chamando uma biblioteca do Node.js, assim como o Import do Python.
+
+// OBS: require() função que importa uma biblioteca no JS
+
+const rl = readline.createInterface({  // rl: Nome que estou atribuindo a interface
+    input: process.stdin, // Entrada de dados por meio do TECLADO
+    output: process.stdout // Saida de dados por meio do TERMINAL
+})
+
+rl.question('Qual o seu Nome? ', (nome) => {
+    rl.question('Qual a sua idade?', (idade) => {
+        rl.question('Qual a linguagem de programação você está estudando?', (linguagem) => {
+            console.log(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`)
+            rl.close() // Fecha a interface de leitura do terminal, ou seja, o programa acabou as perguntas para o usuário
+        })
+    })
+})
+
+// Função question(): Exibe uma pergunta ao usuário, a qual a resposta pode ser armazenada em uma variável através de (var)
+// 
+
